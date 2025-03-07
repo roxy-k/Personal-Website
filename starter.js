ccd8541dd88e1c2a2cba87355ac38b94a65392f6
Oksana Katysheva <katysheva1992@gmail.com>
2025-03-06 21:09:46 -0800

    Update starter.js

--- starter.js
+++ starter.js
@@ -89,3 +89,12 @@ checkLeapYear(2020)
 checkLeapYear(2021)
 checkLeapYear(2024)
 checkLeapYear(1900)
+
+function myFunction(){
+    let i = 0
+    while(i<10){
+        console.log("Hello")
+        i = i + 1 // 0+1; 1+1; 2+1; 3+1; 4+1; 5+1; 6+1; 7+1; 8+1; 9+1
+    }
+}
+myFunction()
